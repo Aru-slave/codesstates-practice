f15c08619bed22d361d72542441b12615ef5ddab
Aru-slave <chlwlgus9584@naver.com>
2022-09-12 17:09:47 +0900

    second

--- Desktop/코드스테이츠/kiosk-program/src/com/codestates/seb/kiosk_program/Kiosk_Program.java
+++ Desktop/코드스테이츠/kiosk-program/src/com/codestates/seb/kiosk_program/Kiosk_Program.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Kiosk_Program {
   //TODO:
   // 메뉴 입력 및 사용자의 수량을 입력하기 위해 Scanner 객체 생성
+  static Scanner sc = new Scanner(System.in);
+  static final int menu_1_cost = 1000;
+  static final int menu_2_cost = 1500;
+  static final int menu_3_cost = 1000;
+  static final int menu_4_cost = 2000;
+  static int menu_count;
 
   /**
    * @menu_n_cost : 메뉴의 가격을 정의 합니다.
@@ -32,7 +38,7 @@ public class Kiosk_Program {
     System.out.println("1) 김밥(1000원) 2) 계란 김밥(1500원) 3) 충무 김밥(1000원) 4) 떡볶이(2000원)");
     //sc == 최 상단에 입력받은 Scanner 객체
     //변수명이 다르다면 하단의 이름을 수정해야 합니다.
-    Scanner sc = new Scanner(System.in);
+
     int number = sc.nextInt();
 
     // 만약 사용자가 입력한 번호의 조건이 아래와 같다면,
@@ -67,15 +73,23 @@ public class Kiosk_Program {
   static void cost(int number) {
     // 안내 문구를 출력합니다.
     //TODO:
+    System.out.println("[안내]선택하신 메뉴의 수량을 입력하여 주세요.");
+    System.out.println("(* 최대 주문 가능 수량 : 99 )");
 
     // 입력받은 수량을 변수에 저장합니다.
     //TODO:
+    menu_count = sc.nextInt();
 
     // 조건에 따라 문구 출력, 재입력을 진행합니다.
     // 조건은 다음과 같습니다.
     // 0 이하 || 99 초과
     // 두 조건 중 하나라도 참인경우 경고 문구 출력과 함께 다시 수량을 입력받을 수 있도록 합니다.
     //TODO:
+    if(menu_count <= 0 || menu_count > 99) {
+      System.out.println(String.format("[경고]%d개는 입력하실 수 없습니다.", menu_count));
+      System.out.println("수량 선택 화면으로 돌아갑니다.");
+      cost(menu_count);
+    }
   }
 
   /**
@@ -83,13 +97,13 @@ public class Kiosk_Program {
    */
   static void output_print(int result) {
     //TODO:
+    System.out.println(String.format("[안내]주문하신 총 금액은 %d원 입니다.",result));
+    System.out.println("[안내]이용해 주셔서 감사합니다.");
   }
 
   /**
    * @main() 메서드 동작
    * 프로그램 실행 -> 메뉴 입력 -> 수량 입력 및 가격 계산 -> 가격 출력
    */
-  public static void main(String[] args) {
-    //TODO:
-  }
+
 }
---
+++ Desktop/코드스테이츠/kiosk-program/src/com/codestates/seb/kiosk_program/Kiosk_start.java
@@ -0,0 +1,14 @@
+package com.codestates.seb.kiosk_program;
+
+
+
+public class Kiosk_start {
+    public static void main(String[] args) {
+        //TODO:
+        Kiosk_Program.input_print();
+        int menu_cost = Kiosk_Program.count();
+        Kiosk_Program.cost(menu_cost);
+        Kiosk_Program.output_print(Kiosk_Program.menu_count * menu_cost);
+
+    }
+}
